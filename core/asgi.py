926e64e3d15f2552a14dfd65e156ceb6636d9231
Shagor Robidas <roysagor88@gmail.com>
2026-06-25 02:11:19 +0600

    fix: asig fix and add websocet route configure

--- core/asgi.py
+++ core/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": application,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            # Include your WebSocket URL patterns here
+        )
+    ),
+})
